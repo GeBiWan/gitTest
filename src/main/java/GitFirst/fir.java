aa556d6e4584f7a65b3bbbebb6b70d270eacdde6
TianXW <327301560qq.com>
2024-04-09 14:33:10 +0800

    第二测试

--- src/main/java/GitFirst/fir.java
+++ src/main/java/GitFirst/fir.java
@@ -7,5 +7,6 @@ package GitFirst;
 public class fir {
         public static void main(String[] args) {
                 System.out.println("Git工具的首次使用");
+                System.out.println("进行git的版本切换测试");
         }
 }
