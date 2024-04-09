1ba2b32aafcde104f0410f613a46faed45635b2c
TianXW <327301560qq.com>
2024-04-09 14:32:21 +0800

    首次提交

---
+++ src/main/java/GitFirst/fir.java
@@ -0,0 +1,11 @@
+package GitFirst;
+
+/**
+ * @author 小维哥
+ * @version 1.2
+ */
+public class fir {
+        public static void main(String[] args) {
+                System.out.println("Git工具的首次使用");
+        }
+}
