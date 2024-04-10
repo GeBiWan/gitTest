eea7e47af0bcfac8b5f54b9b435938950a5628d7
TianXW <327301560qq.com>
2024-04-10 21:07:33 +0800

    hot-fix 1

--- src/main/java/GitFirst/fir.java
+++ src/main/java/GitFirst/fir.java
@@ -10,5 +10,6 @@ public class fir {
                 System.out.println("进行git的版本切换测试");
                 System.out.println("第三次进行版本切换测试");
                 System.out.println("进行分支测试2");
+                System.out.println("进行github的推送");
         }
 }
